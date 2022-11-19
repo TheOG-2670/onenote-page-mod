03212f3a3f9d5b84afdf2b35c6e8add783e39c95
TheOG-2670 <ogolan3589@gmail.com>
2022-11-19 11:34:02 -0500

    initial commit

---
+++ OneNoteTest/OneNoteTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneNoteTest
+{
+    internal class OneNoteTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
