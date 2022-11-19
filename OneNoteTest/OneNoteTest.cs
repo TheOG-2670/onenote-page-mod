671efd1a0063f01d86b12eef587d92be018a2130
TheOG-2670 <ogolan3589@gmail.com>
2022-11-19 11:35:25 -0500

    accessed onenote xml namespace name

--- OneNoteTest/OneNoteTest.cs
+++ OneNoteTest/OneNoteTest.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.OneNote;
+using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace OneNoteTest
 {
     internal class OneNoteTest
     {
+        static Application app;
+        static XNamespace xNameSpace;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            app = new Application();
+
+            GetNameSpace();
+            Console.WriteLine(xNameSpace);
+        }
+
+        //get notebook's xml namespace used for traversing the notebook tree and searching for nodes (namespace + nodeName)
+        private static void GetNameSpace()
+        {
+            string notebookXml;
+
+            app.GetHierarchy(null, HierarchyScope.hsNotebooks, out notebookXml);
+            XDocument doc = XDocument.Parse(notebookXml);
+            xNameSpace = doc.Root.Name.Namespace;
         }
     }
 }
