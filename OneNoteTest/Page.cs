4c35151f2ad19adccd9074ad51dc5305716eb871
TheOG-2670 <ogolan3589@gmail.com>
2022-12-11 14:53:59 -0500

    removed page body retrieval; called page element retrieval from constructor; added condition for page title text

--- OneNoteTest/Page.cs
+++ OneNoteTest/Page.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Office.Interop.OneNote;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -7,11 +8,12 @@ namespace OneNoteTest
     public class Page
     {
         private string pageId;
-        private XElement page, pageTitle, pageBody;
+        private XElement page, pageTitle;
 
         public Page(string id)
         {
             pageId = id;
+            GetPageElements();
         }
 
         public string Id
@@ -21,31 +23,24 @@ namespace OneNoteTest
 
         public XElement Title
         {
-            set { pageTitle = value; }
             get { return pageTitle; }
         }
 
-        public XElement Body
+        internal void GetPageElements()
         {
-            set { pageBody = value; }
-            get { return pageBody; }
-        }
-
-        public void GetPageElements()
-        {
-            string pageXml;
-            Utils.AppInstance.GetPageContent(pageId, out pageXml, PageInfo.piAll);
+            Utils.AppInstance.GetPageContent(pageId, out string pageXml, PageInfo.piAll);
 
             XDocument pageDoc = XDocument.Parse(pageXml);
             page = pageDoc.Descendants(Utils.GetNameSpace() + "Page").First();
             XElement pageTitle_ = page.Descendants(Utils.GetNameSpace() + "Title").First();
-            XElement pageOutline_ = page.Descendants(Utils.GetNameSpace() + "Outline").First();
-            XElement pageBodyText_ = pageOutline_.Descendants(Utils.GetNameSpace() + "T").First();
             XElement pageTitleOE_ = pageTitle_.Descendants(Utils.GetNameSpace() + "OE").First();
             XElement pageTitleText_ = pageTitleOE_.Descendants(Utils.GetNameSpace() + "T").First();
+            if(string.IsNullOrEmpty(pageTitleText_.Value))
+            {
+                pageTitleText_= (XElement)pageTitleOE_.Descendants(Utils.GetNameSpace() + "T").First().NextNode;
+            }
 
             pageTitle = pageTitleText_;
-            pageBody = pageBodyText_;
         }
 
         public void UpdateTitle(string newPageTitle)
